13fefb75ddb6869a6d818b7acb5b064747ec2738
gioocharkviani <gioocharkviani@gmail.com>
2024-09-21 11:02:05 +0400

    frs

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <div className="text-center">
         <h1 className="text-6xl font-bold mb-4">404</h1>
         <h2 className="text-2xl font-semibold mb-6">Oops! Page Not Found</h2>
-        <p className="mb-8">The page you're looking for doesn't exist or has been moved.</p>
+        <p className="mb-8">The page you&apos;re looking for doesn&apos;t exist or has been moved.</p>
         <Link
           href="/"
           className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
