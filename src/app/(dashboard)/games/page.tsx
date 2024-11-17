3f69a1e7bc6999a3f07a94135ba45f16ded1f5d2
gioocharkviani <gioocharkviani@gmail.com>
2024-11-17 14:10:34 +0400

    update table component and remove global state

---
+++ src/app/(dashboard)/games/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewTableTest from './NewTableTest';
+
+const page = () => {
+  return (
+    <div className="bg-white dark:bg-darkBlue p-2 rounded-lg shadow-lg">
+      <NewTableTest />
+    </div>
+  );
+};
+
+export default page;
